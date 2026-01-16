f27bdc2ac2b7de73fdf0b31ece9532d3e6166264
Hai Liang Wang <hai@chatopera.com>
2026-01-16 19:39:26 +0800

    Update contents

---
+++ bin/commit.sh
@@ -0,0 +1,26 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd -P `dirname "$0"`;pwd)
+export PYTHONUNBUFFERED=1
+export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
+
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+git add --all
+git commit -m "Update contents"
+git pull origin master -ff
+
+if [ ! $? -eq 0 ]; then
+    echo "Failed, fix manullay"
+    exit 1
+fi
+
+git push origin master
+git push github master
+echo "Updated contents"
