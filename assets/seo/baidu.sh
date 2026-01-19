b593cee31fe32948431881ed15391fb11db139d0
Hai Liang Wang <hai@chatopera.com>
2026-01-19 08:56:06 +0800

    Update contents

---
+++ assets/seo/baidu.sh
@@ -0,0 +1,32 @@
+#! /bin/bash 
+###########################################
+#
+###########################################
+
+# constants
+baseDir=$(cd `dirname "$0"`;pwd)
+cwdDir=$PWD
+export PYTHONNOUSERSITE=1
+export PYTHONUNBUFFERED=1
+#export PATH=/opt/miniconda3/envs/venv-py3/bin:$PATH
+export TS=$(date +%Y%m%d%H%M%S)
+export DATE=`date "+%Y%m%d"`
+export DATE_WITH_TIME=`date "+%Y%m%d-%H%M%S"` #add %3N as we want millisecond too
+
+# functions
+
+# main 
+[ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
+
+cd $baseDir
+
+if [ ! -e localrc ]; then
+    echo "File not found, localrc"
+    exit 1
+fi
+
+source localrc
+
+pwd
+set -x
+curl -H 'Content-Type:text/plain' --data-binary @baidu_urls.txt "http://data.zz.baidu.com/urls?site=https://mendelow.chatopera.com&token=${BAIDU_TOKEN}"
